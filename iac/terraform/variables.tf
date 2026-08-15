1f8126e09b349efa5fedeee6d97c9e35778cec91
mohdazam193 <aazam.mohammad193@gmail.com>
2026-08-15 13:04:13 +0530

    Remove IaC setup temporarily

--- iac/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "tenancy_ocid" { type = string }
-variable "user_ocid" { type = string }
-variable "fingerprint" { type = string }
-variable "private_key_path" { type = string }
-variable "region" { type = string }
-variable "compartment_id" { type = string }
-variable "availability_domain" { type = string }
