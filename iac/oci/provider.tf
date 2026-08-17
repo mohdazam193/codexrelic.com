a7774b86d59d4a209e44005536db7deb338b4f7d
mohdazam193 <aazam.mohammad193@gmail.com>
2026-08-17 18:24:24 +0530

    fix: switch terraform provider backend to OCI S3

--- iac/oci/provider.tf
+++ iac/oci/provider.tf
@@ -6,8 +6,15 @@ terraform {
     }
   }
 
-  backend "azurerm" {
+  backend "s3" {
     # Backend configuration will be injected during terraform init by Azure Pipelines
+    # OCI Object Storage requires these specific flags to work with Terraform's S3 backend
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_sso_auth               = true
+    skip_metadata_api_check     = true
   }
 }
 
